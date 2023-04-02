ec2c7780ca99244c57995aef6f82ca3e710f6421
Lokesh0211 <peddamallalokesh549@gmail.com>
2023-04-02 16:06:57 +0530

    second commit

--- School_Management/src/Main/Main.java
+++ School_Management/src/Main/Main.java
@@ -4,23 +4,42 @@ public class Main {
 public static void main(String[] args) {
 	Student stu = new Student();
 	stu.createTable();
-	System.out.println("SELECT YOUR CHOICE");
-	System.out.println("1. ADD STUDENT \n2. SHOW STUDENT DETAILS \n3. UPDATE STUDENT DETAILS \n4. DELETE STUDENT DETAILS \n5. DISPLAY ALL STUDENT DETAILS");
-	
 	Scanner sc = new Scanner(System.in);
-	int choice = sc.nextInt();
-	switch(choice) {
-	case 1: stu.addStudentDetails();
-	break;
-	case 2: stu.showStudentDetails();
-	break;
-	case 3: stu.updateStudentDetails();
-	break;
-	case 4: stu.deleteStudentDetails();
-	break;
-	case 5: stu.displayAllStudentDetails();
-	break;
-	}
-	sc.close();
+	  int choice = 0;
+      while (choice != 6) {
+          System.out.println("SELECT YOUR CHOICE");
+          System.out.println("1. ADD STUDENT \n2. SHOW STUDENT DETAILS \n3. UPDATE STUDENT DETAILS \n4. DELETE STUDENT DETAILS \n5. DISPLAY ALL STUDENT DETAILS \n6. Exit");
+
+          choice = sc.nextInt();
+
+          switch(choice) {
+              case 1: 
+                  stu.addStudentDetails();
+                  System.out.println("\n");
+                  break;
+              case 2: 
+                  stu.showStudentDetails();
+                  System.out.println("\n");
+                  break;
+              case 3: 
+                  stu.updateStudentDetails();
+                  System.out.println("\n");
+                  break;
+              case 4: 
+                  stu.deleteStudentDetails();
+                  System.out.println("\n");
+                  break;
+              case 5: 
+                  stu.displayAllStudentDetails();
+                  System.out.println("\n");
+                  break;
+              case 6: 
+                  break;
+              default: 
+                  System.out.println("Enter correct choice");
+                  break;
+          }
+      }
+      sc.close();
 }
 }
